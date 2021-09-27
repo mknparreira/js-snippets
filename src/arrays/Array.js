39210c40f8ec96720c1b0f1216b913cb73307324
Maikon Parreira <mknparreira@gmail.com>
2021-09-27 14:04:22 +0100

    Added Array folder and first array snippet

---
+++ src/arrays/Array.js
@@ -0,0 +1,9 @@
+// You can easily remove duplicates with Set in JavaScript. Its a life saver.
+// const arrayDuplicated = [1, 2, 3, 4, 4, 5, 5, 6];
+// console.log(removeDuplicates(arrayDuplicated));
+// Result: [ 1, 2, 3, 4, 5, 6 ];
+export const removeDuplicates = (arr) => [...new Set(arr)];
+
+export function send() {
+  return true;
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { removeDuplicates } from './arrays/Array.js';
+
+console.log('TESTING REMOVE DUPLICATES ==>');
+const arrayDuplicated = [1, 2, 3, 4, 4, 5, 5, 6];
+console.log(removeDuplicates(arrayDuplicated));
+console.log('END OF REMOVE DUPLICATES <==');
