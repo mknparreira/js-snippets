29a16b951cf5ea168bbb3d2aa5513bc74fb8d919
Maikon Parreira <mknparreira@gmail.com>
2022-03-01 15:15:52 +0000

    Updated reference

--- src/index.js
+++ src/index.js
@@ -151,4 +151,5 @@ console.log(compareObjects(premierLeagueClubB, premierLeagueClubC));
 https://medium.com/@sbsurjeet66/13-javascript-array-methods-bf3df9cdeab0
 https://medium.com/@sandeepbanuka2209/5-must-know-javascript-one-liners-e1aad834852a
 https://nikakharebava.medium.com/javascript-array-methods-you-must-know-examples-5ffd90b10dad
+https://ranamahmud.medium.com/10-tricky-things-in-javascript-1ce760c5fc33
 */
