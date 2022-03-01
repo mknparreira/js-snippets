a0635219c719b9f69c8867e0b2803b8cfed1d0da
Maikon Parreira <mknparreira@gmail.com>
2022-03-01 00:15:47 +0000

    Added faker package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import {
   removeDuplicates,
   isValueInArray,
@@ -13,59 +14,59 @@ import displayValuesAsObject from './library/Console.js';
 import { convertArrayToAnObject, isObject, compareObjects } from './library/Object.js';
 import { isNumber, averageNum } from './library/Number.js';
 
-console.info('---------- Remove duplicated values inside an array ----------');
+console.info('----------1. Remove duplicated values inside an array ----------');
 const arrayDuplicated = [1, 2, 3, 4, 4, 5, 5, 6];
 const removeDups = removeDuplicates(arrayDuplicated);
 console.log(removeDups);
 
-console.info('---------- Verifing if value exists inside an array ----------');
-const names = ['Limon', 'Mahbub', 'Alam', 'Tanvir'];
+console.info('----------2. Verifing if value exists inside an array ----------');
+const names = faker.datatype.array(3);
 const isInclude = isValueInArray('e', names);
-console.log(isInclude);
+console.log(isInclude); // Returns false
 
-console.info('---------- Array count values ----------');
+console.info('----------3. Array count values ----------');
 const arrayValues = [3, 5, 3, 'foo', 'bar', 'foo'];
 const countingArrayValues = arrayCountValues(arrayValues);
 console.table(countingArrayValues);
 
-console.info('---------- Find the max or the min values inside an array ----------');
+console.info('----------4. Find the max or the min values inside an array ----------');
 const arrayExampleFindMaxAndMinValues = [2, 8, 15, 4];
 console.log(Math.max(...arrayDuplicated));
 console.log(Math.min(...arrayExampleFindMaxAndMinValues));
 
-console.info('---------- Shorthand object prperty assignment ----------');
+console.info('----------5. Shorthand object prperty assignment ----------');
 displayValuesAsObject('Maikon Parreira', 'mknparreira@gmail.com', true);
 
-console.info('---------- Convert an array to an object ----------');
-const arrayPlanets = ['Saturn', 'Earth', 'Venus', 'Uranus', 'Mercury'];
+console.info('----------6. Convert an array to an object ----------');
+const arrayPlanets = faker.datatype.array(5);
 const objPlanets = convertArrayToAnObject(arrayPlanets);
 console.log(objPlanets);
 
-console.info('---------- Merge arrays ----------');
+console.info('----------7. Mergging arrays ----------');
 const planets1 = ['Saturn', 'Earth', 'Uranus', 'Mercury'];
 const planets2 = ['Venus', 'Earth', 'Mars', 'Jupiter'];
 const arrayMergeResult = arrayMerge(planets1, planets2);
 console.log(arrayMergeResult);
 
-console.info('---------- Array intersect ----------');
+console.info('----------8. Array intersect ----------');
 const arrIntersect1 = ['Saturn', 'Earth', 'Uranus', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter'];
 const arrIntersect2 = ['Saturn', 'Earth', 'Uranus'];
 const arrayIntersectResult = arrayIntersect(arrIntersect1, arrIntersect2);
 console.log(arrayIntersectResult);
 
-console.info('---------- Remove False values in an array ----------');
+console.info('----------9. Remove False values in an array ----------');
 const falseValues = ['0', '', null, 0, false, 'A', 'B', undefined, NaN, true, 5];
 
 const arrayRemoveFalseValues = removeFalseValues(falseValues);
 console.log(arrayRemoveFalseValues);
 
-console.info('---------- Is number ----------');
+console.info('----------10. Is number ----------');
 console.log(isNumber(1.5));
 
-console.info('---------- Average value ----------');
+console.info('----------11. Average value ----------');
 console.log(averageNum(55, 8, 37, 4));
 
-console.info('---------- Array Map ----------');
+console.info('----------12. Array Map ----------');
 const people = [
   { name: 'Todd', position: 'Accountant' },
   { name: 'Jessica', position: 'Secretary' },
@@ -75,13 +76,13 @@ const people = [
 const positions = people.map((person) => person.position);
 console.log(positions);
 
-console.info('---------- Updating all the elements of an array ----------');
+console.info('----------13. Updating all the elements of an array ----------');
 const usersBalance = [
   { name: 'Jane', balance: 100.00 },
   { name: 'John', balance: 55.25 },
 ];
 
-// define pure methods for our `map`
+// Define pure methods for our `map`
 const double = (amount) => amount * 2;
 const doubleUserBalance = (user) => ({
   ...user,
@@ -92,7 +93,7 @@ const doubleUserBalance = (user) => ({
 const usersWithDoubledBalance = usersBalance.map(doubleUserBalance);
 console.log(usersWithDoubledBalance);
 
-console.info('---------- Array Filter ----------');
+console.info('----------14. Array Filter ----------');
 const applicants = [
   { name: 'James', age: 25 },
   { name: 'John', age: 23 },
@@ -104,12 +105,12 @@ const applicants = [
 const qualified = applicants.filter((person) => person.age >= 18);
 console.log(qualified);
 
-console.info('---------- Array Reduce ----------');
+console.info('----------15. Array Reduce ----------');
 const numbersToReduce = [30, 40, 50, 20];
 const resultReduce = numbersToReduce.reduce((previousValue, currentValue) => previousValue + currentValue);
 console.log(resultReduce);
 
-console.info('---------- Array Every ----------');
+console.info('----------16. Array Every ----------');
 console.info('Tests whether all elements in the array pass the condition. It returns a Boolean value.');
 console.info('Array.every() all values pass. Array.some() at least one');
 const applicantsEvery = [25, 13, 6, 8, 14, 59, 56, 2, 40];
@@ -117,7 +118,7 @@ const qEvery = applicantsEvery.every((num) => num >= 10);
 console.log(qEvery);
 
 console.info('---------- Array Every ----------');
-console.info('Checking that all values satisfy a group of champions.');
+console.info('17. Checking that all values satisfy a group of champions.');
 const champions = [
   { name: 'Darius', level: 7 },
   { name: 'Katarina', level: 12 },
@@ -129,19 +130,25 @@ const hasUltimate = hasExpectedLevel(6);
 const chamionsAllHaveUltimate = champions.every(hasUltimate);
 console.log(chamionsAllHaveUltimate);
 
-console.info('---------- Array sort by Asc ----------');
+console.info('----------18. Array sort by Asc ----------');
 const arrSortAsc = [25, 13, 6, 8, 14, 59, 56, 2, 40];
 console.log(arraySortByAsc(arrSortAsc));
 
-console.info('---------- Array sort by Desc ----------');
+console.info('----------19. Array sort by Desc ----------');
 const arrSortDesc = [25, 13, 6, 8, 14, 59, 56, 2, 40];
 console.log(arraySortByDesc(arrSortDesc));
 
-console.info('---------- Is object ----------');
+console.info('----------20. Is object ----------');
 const premierLeagueClub = { name: 'Chelsea', points: 7 };
 console.log(isObject(premierLeagueClub));
 
-console.info('---------- Compare objects ----------');
+console.info('----------21. Compare objects ----------');
 const premierLeagueClubB = { name: 'Arsenal', points: 7 };
 const premierLeagueClubC = { name: 'City', points: 7 };
 console.log(compareObjects(premierLeagueClubB, premierLeagueClubC));
+
+/* REFERENCE
+https://medium.com/@sbsurjeet66/13-javascript-array-methods-bf3df9cdeab0
+https://medium.com/@sandeepbanuka2209/5-must-know-javascript-one-liners-e1aad834852a
+https://nikakharebava.medium.com/javascript-array-methods-you-must-know-examples-5ffd90b10dad
+*/
