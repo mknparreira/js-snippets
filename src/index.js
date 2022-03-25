a8f14843dbaa051525eeea54bb9d5f1724a628b3
Maikon Parreira <mknparreira@gmail.com>
2022-03-25 14:33:50 +0000

    Update reference

--- src/index.js
+++ src/index.js
@@ -152,4 +152,6 @@ https://medium.com/@sbsurjeet66/13-javascript-array-methods-bf3df9cdeab0
 https://medium.com/@sandeepbanuka2209/5-must-know-javascript-one-liners-e1aad834852a
 https://nikakharebava.medium.com/javascript-array-methods-you-must-know-examples-5ffd90b10dad
 https://ranamahmud.medium.com/10-tricky-things-in-javascript-1ce760c5fc33
+https://hidaytrahman.medium.com/9-array-methods-every-javascript-developer-should-know-104d21ae7b8
+https://medium.com/@jitendradprajapati/mistakes-i-have-made-as-a-js-noob-developer-bb58d89a3e75
 */
